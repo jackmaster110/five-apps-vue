2f67ae50358ff316b93ea540b6986f9929c362a1
Samuel Shear <dspfgh34@gmail.com>
2021-08-17 17:05:19 -0400

    Finished

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,114 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="container">
+    <header>
+      <h1>Notes App</h1>
+    </header>
+    <main>
+      <Note
+        v-for="(note, index) in notes"
+        :key="index"
+        :title="note.title"
+        :body="note.body"
+      />
+      <div class="note">
+        <form @submit="submitForm()">
+          <input type="text" placeholder="Note Title" v-model="title" />
+          <textarea placeholder="Note Body" v-model="body"></textarea>
+          <input type="submit" value="Add Note" />
+        </form>
+      </div>
+    </main>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import Note from "./components/Note.vue";
+
+type NoteType = {
+  title: string;
+  body: string;
+};
 
 @Component({
   components: {
-    HelloWorld,
+    Note,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  notes: Array<NoteType> = [];
+  title!: string;
+  body?: string;
+
+  constructor() {
+    super();
+    const data = localStorage.getItem("notes");
+    if (data) this.notes = JSON.parse(data);
+  }
+
+  submitForm(): void {
+    let note: NoteType = {
+      title: this.title,
+      body: this.body || "",
+    };
+    this.notes.push(note);
+    localStorage.setItem("notes", JSON.stringify(this.notes));
+  }
+}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+}
+
+.container {
+  width: 100%;
+  height: auto;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+
+  header {
+    display: flex;
+    align-items: center;
+
+    width: 100%;
+    height: 56px;
+    background-color: #4e78b8;
+    color: white;
+
+    h1 {
+      margin-left: 6px;
+    }
+  }
+
+  main {
+    margin: 10px;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+    grid-gap: 1rem;
+    align-items: center;
+
+    .note {
+      display: flex;
+      flex-direction: column;
+
+      padding: 10px;
+      background-color: #a15fbb;
+      border-radius: 5px;
+
+      form {
+        display: flex;
+        flex-direction: column;
+
+        textarea {
+          resize: none;
+        }
+      }
+    }
+  }
 }
 </style>
---
+++ src/components/Note.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="note">
+    <h4>{{ this.title }}</h4>
+    <span>{{ this.body }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {},
+})
+export default class App extends Vue {
+  @Prop() title!: string;
+  @Prop() body?: string;
+}
+</script>
